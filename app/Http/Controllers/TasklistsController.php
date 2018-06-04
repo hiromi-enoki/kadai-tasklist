21110b17eaee1ed944a0ab21d21f7d3a66354947
hiromi.enoki <prismblaqars@gmail.com>
2018-06-04 03:28:41 +0000

    add column

--- app/Http/Controllers/TasklistsController.php
+++ app/Http/Controllers/TasklistsController.php
@@ -45,7 +45,14 @@ class TasklistsController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+            ]);
+            
+            
         $tasklist = new Tasklist;
+        $tasklist->status = $request->status;
         $tasklist->content = $request->content;
         $tasklist->save();
 
@@ -91,7 +98,14 @@ class TasklistsController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request,[
+            'status'=>'required|max:10',
+            'content'=>'required|max:191',
+            ]);
+            
+            
         $tasklist = Tasklist::find($id);
+        $tasklist->status = $request->status;
         $tasklist->content = $request->content;
         $tasklist->save();
 
--- resources/views/tasklists/index.blade.php
+++ resources/views/tasklists/index.blade.php
@@ -7,10 +7,11 @@
     @if (count($tasklists) > 0)
         <ul>
             @foreach ($tasklists as $tasklist)
-               <li>{!! link_to_route('tasklists.show', $tasklist->id, ['id' => $tasklist->id]) !!} : {{ $tasklist->content }}</li>
+               <li>{!! link_to_route('tasklists.show', $tasklist->id, ['id' => $tasklist->id]) !!} :{{ $tasklist->status }} > {{ $tasklist->content }}</li>
             @endforeach
         </ul>
     @endif
+    
     {!! link_to_route('tasklists.create', '新規タスクの投稿') !!}
 
 @endsection
